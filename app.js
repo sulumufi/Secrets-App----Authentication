ed2ea2590a18c5718546cadd9091aeab46551b43
sulu mufi <suluksm@gmail.com>
2021-08-31 11:00:39 +0530

    Level 5_e

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+
 
 
 //passport module code
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
-
-const mongoose = require("mongoose");
 
 //passport module code
 app.use(session({
@@ -22,12 +24,12 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
   }))
+
 app.use(passport.initialize())
 app.use(passport.session());
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 url = "mongodb://localhost:27017/userDB";
-
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -38,37 +40,68 @@ mongoose.set('useCreateIndex', true);
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId : String
 });
 
 
 //passport module code
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 
 //passport module code with the User model created with a schema using a plugin of passpoert-mongoose-local,
 //and the same model is been passed below!.
-
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 
 app.get("/", function (req, res) {
     res.render("home");
-    
 });
+
+app.get("/auth/google",passport.authenticate("google",{scope : ["profile"]}) );
+
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+
+
 app.get("/login", function (req, res) {
     res.render("login");
 });
@@ -81,7 +114,6 @@ app.get("/logout",function(req, res){
     res.redirect("/");
 })
 
-
 app.get("/secrets", function(req,res){
     if(req.isAuthenticated()){
         res.render("secrets")
@@ -89,6 +121,8 @@ app.get("/secrets", function(req,res){
         res.redirect("/login");
     }
 })
+
+
 
 
 
@@ -129,3 +163,6 @@ app.post("/login", function (req, res) {
 app.listen(3000, function () {
     console.log("Server Up and running!");
 });
+
+
+
